c130fbc72cf5775dabe443aeeb0e1ba1c742e005
JhoannS <sebastianzamudio2004@gmail.com>
2024-04-15 11:08:34 -0500

    Add dependences

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import Handlebars from 'handlebars';
 import{verifyDates} from "../controllers/login.js"
 
 export const routerLogin = Router();
 
+routerLogin.get("/", verifyDates);
 /***************  VALIDACION DE AUTENTICIDAD DE USUARIO  ***************/
 routerLogin.post("/", verifyDates);
